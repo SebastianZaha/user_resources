2f8fe180b90d87cc1f6e531a1ba72f641e69264f
Sebastian Zaha <sebastian.zaha@gmail.com>
2014-03-07 15:58:40 +0100

    Implement attr_immutable and before_attributes_set callback. Docs.

--- lib/user_resources/model.rb
+++ lib/user_resources/model.rb
@@ -1,19 +1,44 @@
-# Classes using this mixin should implement the following methods:
+# Requires:
+#   editable_by?(user) -> Boolean
+#     * true if `user` is allowed (according to the application's business logic) to edit
+#       this resource
 #
-# editable_by?(user) -> Boolean
-#   * true if `user` is allowed (according to the application's business logic) to edit
-#     this resource
-# save -> Boolean
-#   * should return false when the object cannot be saved (invalid, etc)
-#   * if the class is already an ActiveRecord model, you already have this
+#  Required (possibly) - The following are also required but already provided by ActiveRecord:
+#
+#   * save -> Boolean
+#     * should return false when the object cannot be saved (invalid, etc)
+#     * if the class is already an ActiveRecord model, you already have this
+#   * persisted? -> Boolean
+#   * attributes=(Hash)
+#
+# Provides:
+#   * self.user_performing_update -> Object (whatever the caller passes to user_updates)
+#   * self.attributes_from_client -> Hash
+#   * self.attr_immutable(Array<Symbol> attribute_names)
+#   * self.before_attributes_set(Symbol method_name)
+#     - requires: method(Hash attributes) -> Hash
+#     - method will be called before client sent attributes are set. It should return a set of
+#       pre-processed attributes if it needs to pre-process them
+#
+#   * user_update(Hash attrs, Object user)
+#   * user_destroy(Object user)
 module UserResources::Model
 
   def self.included(base)
     base.class_eval do
+
       # The user that is changing this object through our protected methods.
       attr_accessor :user_performing_update
       # The attributes he is trying to set.
       attr_accessor :attributes_from_client
+
+      def self.attr_immutable(*symbols)
+        instance_variable_set(:@attr_immutable, symbols)
+      end
+
+      def self.before_attributes_set(method_name)
+        instance_variable_set(:@before_attr_set, method_name)
+      end
     end
   end
 
@@ -22,11 +47,11 @@ module UserResources::Model
   def user_update(attrs, user)
     raise UserResources::Forbidden unless editable_by?(user)
     self.user_performing_update = user
-    self.attributes_from_client = attrs
 
-    before_set_attributes if respond_to?(:before_set_attributes, true)
+    callback = self.class.instance_variable_get(:@before_attr_set)
+    self.attributes_from_client = callback ? self.send(callback, attrs) : attrs
 
-    self.attributes = attrs
+    self.attributes = sanitize_immutable(attributes_from_client)
 
     raise UserResources::Forbidden unless editable_by?(user)
 
@@ -49,5 +74,18 @@ module UserResources::Model
 
     self.user_performing_update = self.attributes_from_client = nil
     self
+  end
+
+
+  protected
+
+  def sanitize_immutable(attrs)
+    immutable = self.class.instance_variable_get(:@attr_immutable)
+
+    if persisted? && immutable
+      attrs.reject { |k, v| immutable.include?(k)  }
+    else
+      attrs
+    end
   end
 end
--- test/user_resources/model_test.rb
+++ test/user_resources/model_test.rb
@@ -2,39 +2,94 @@ require 'test_helper'
 
 class ModelTest < Test::Unit::TestCase
 
-  class DummyModel
-    include UserResources::Model
-
-    attr_accessor :attributes
-  end
-
 
   def test_exceptions
-    m = DummyModel.new
     attrs = {name: 'Hello'}
 
-    # Stub methods. Model is not editable, but save would return true.
-    def m.editable_by?(user) false end
-    def m.save() true end
-
+    m = DummyNotEditable.new
     msg = 'Model should raise a forbidden exception when it is not editable by this user.'
     assert_raise(UserResources::Forbidden, msg) do
       m.user_update(attrs, :someone)
     end
 
-    # Stub methods. Model is editable by :someone, but save returns false.
-    def m.editable_by?(user) user == :someone end
-    def m.save() false end
-
+    m = DummyNotValid.new
     msg = 'Model should raise an invalid exception when it cannot be saved.'
     assert_raise(UserResources::Invalid, msg) do
       m.user_update(attrs, :someone)
     end
 
     # Now it's both valid, and editable
-    def m.save() true end
-
+    m = DummyModel.new
     m.user_update(attrs, :someone)
     assert_equal(m.attributes, attrs, 'Should set attributes on the model')
+  end
+
+  def test_before_attributes_set
+    m = DummyModel.new
+    attrs = {id: 1, illegal_attr: true}
+
+    def m.preprocess_attributes(attrs)
+      attrs.delete(:illegal_attr)
+      attrs
+    end
+
+    m.user_update(attrs, :someone)
+
+    assert_equal(1, m.attributes[:id])
+    assert(!m.attributes[:illegal_attr], 'Our preprocess_attributes callback should be called')
+  end
+
+  def test_immutable_attributes
+    attrs = {id: 1, not_mut: 2}
+
+    m = DummyWithImmutable.new.user_update(attrs, :someone)
+    assert_equal(1, m.attributes[:id])
+    assert_equal(2, m.attributes[:not_mut])
+
+    # Fake that we saved the model now
+    def m.persisted?() true end
+    m.user_update({id: 11, not_mut: 12}, :someone)
+
+    assert_equal(11, m.attributes[:id], 'The mutable attribute should change')
+
+    # NOTE that the attributes = method in our dummy resets all attributes always.
+    # The 'attributes=' method in Rails only considers a smart diff (see assign_attributes in
+    # ActiveRecord)
+    assert_equal(nil, m.attributes[:not_mut], 'The immutable attribute was reset')
+  end
+
+
+  class DummyModel
+    include UserResources::Model
+
+    attr_accessor :attributes
+
+    before_attributes_set :preprocess_attributes
+
+    def editable_by?(user); true end
+    def save(); true end
+    def persisted?() false end
+
+    protected
+
+    def preprocess_attributes(attrs)
+      attrs
+    end
+  end
+
+  class DummyNotEditable < DummyModel
+    def editable_by?(user)
+      false
+    end
+  end
+
+  class DummyNotValid < DummyModel
+    def save
+      false
+    end
+  end
+
+  class DummyWithImmutable < DummyModel
+    attr_immutable :not_mut
   end
 end
